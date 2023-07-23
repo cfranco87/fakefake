5ea4258b6877e20ce4ba8d6fd42034ebb16e24a7
cfranco87 <carlosfrancoart@gmail.com>
2023-07-22 19:00:02 -0700

    s

--- print_d.c
+++ print_d.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include <limits.h>
+
 /**
  *print_d - prints an integer in base 10
  *@args: the argumetns it receives
@@ -13,13 +15,21 @@ int print_d(va_list args)
 	int p = 0;
 	int count = 0;
 
+	if (num == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
 	if (num < 0)
 	{
-		num = -num;
 		_putchar('-');
 		count++;
+		if (num == INT_MIN)
+		{
+			digit[p++] = '0' + (num % 10) * -1;
+			num = -(num / 10);
+		}
 	}
-
 	do {
 		digit[p++] = '0' + (num % 10);
 		num /= 10;
